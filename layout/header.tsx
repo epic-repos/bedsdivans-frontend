39856957edc764f4ca519c3cc611c1f1611ba377
1235790hhdxcjkkjffddfcbjjjhvv <krishnakmar555@gmail.com>
2022-07-22 16:55:27 +0530

    krishnanew

--- layout/header.tsx
+++ layout/header.tsx
@@ -2,103 +2,226 @@ import React from "react";
 import type { NextPage } from "next";
 import Styles from "./headerstyle/topheader.module.scss";
 import Link from "next/link";
- 
 
 const Header = () => {
   return (
     <div>
-    <section className={Styles.headersection}>
-      <div className={`${Styles.headermain1} container`}>
-        <div className={`${Styles.headermain} row`}>
-          <div className={Styles.topheader}>
-            <div className={Styles.firstbox}>
-              <p>Free Delivery over £40 to most of UK*</p>
-            </div>
-            <div className={Styles.secondbox}>
-              <ul>
-                <li>Contact Us</li>
-                <li>About Us</li>
-                <li>9560321245</li>
-              </ul>
+      <section className={Styles.headersection}>
+        <div className={`${Styles.headermain1} container`}>
+          <div className={`${Styles.headermain} row`}>
+            <div className={Styles.topheader}>
+              <div className={Styles.firstbox}>
+                <p>Free Delivery over £40 to most of UK*</p>
+              </div>
+              <div className={Styles.secondbox}>
+                <ul>
+                  <li>Contact Us</li>
+                  <li>About Us</li>
+                  <li>9560321245</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
 
-    <section className={Styles.secondheader}>
-       <div className="container">
+      <section className={Styles.secondheader}>
+        <div className="container">
           <div className="row">
-             <div className="col-md-2">
-             <div className={Styles.mainlogo}>
+            <div className="col-md-2">
+              <div className={Styles.mainlogo}>
                 <img src="/image/1.png" alt="logo" />
-             </div>
-             </div>
-             <div className={`${Styles.searchbar1} col-md-7`}>
-             <div className={Styles.searchbar}>
-              <input type="text" placeholder="search for products"/>
-             
-              <select name="category" id="category">
-           <option value="Select">Select Category</option>
-          <option value="1">1</option>
-             <option value="2">2</option>
-            <option value="3">3</option>
-  </select>
+              </div>
+            </div>
+            <div className={`${Styles.searchbar1} col-md-7`}>
+              <div className={Styles.searchbar}>
+                <input type="text" placeholder="search for products" />
 
-  <button className={Styles.search_button}>
-                <img src="/image/SEARCH.SVG" alt="" />
-              </button>
-             </div>
-             </div>
-             <div className="col-md-3">
+                <select name="category" id="category">
+                  <option value="Select">Select Category</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                </select>
+
+                <button className={Styles.search_button}>
+                  <img src="/image/SEARCH.SVG" alt="" />
+                </button>
+              </div>
+            </div>
+            <div className="col-md-3">
               <div className={Styles.carticon}>
-                  <ul>
-                    <li>
-                  <div className={Styles.wishlist}>
-                  <img src="/image/HEART.SVG" alt="icon" />
-                  <span>2</span>
-                  </div>
-                    </li>
-                    <li>
-                      <div className={Styles.wishlist1}>
+                <ul>
+                  <li>
+                    <div className={Styles.wishlist}>
+                      <img src="/image/HEART.SVG" alt="icon" />
+                      <span>2</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className={Styles.wishlist1}>
                       <img src="/image/CART.SVG" alt="icon" />
                       <span>2</span>
-                      </div>                      
-                    </li>
-                  </ul>
-              </div>
-             </div>
-          </div>
-       </div>
-    </section >
-
- 
-
-
-    <section className={Styles.menudiv}>
-          <div className="container">
-             <div className="row">
-                <div className="col-md-12">
-                    <div className={Styles.mainmenu}>
-                      <ul>
-                         <li>Beds  </li>
-                         <li>Mattresses</li>
-                         <li>Wardrobes</li>
-                         <li>Sofa Beds</li>
-                         <li>Divan Beds</li>
-                         <li>Kids</li>
-                         <li>Furniture</li>
-                         <li>Finance</li>
-                         <li>Clearance</li>
-                         <li>Help & Advice</li>
-                      </ul>
                     </div>
-                </div>
-             </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
-    </section>
+        </div>
+      </section>
+
+      <section className={Styles.menudiv}>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className={Styles.mainmenu}>
+                <ul>
+                  <li>
+                    <Link href="/divanbed">
+                      <a>Divan Beds</a>
+                    </Link>
+                    <ul>
+                    <section className="drop">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3">
+            <div className={Styles.dropdownmenu}>
+              <img src="/image/bettersleepimage.webp" alt="img" />
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className={Styles.dropdownmenu}>
+              <h2>Shop BY Type</h2>
+              <ul>
+                <li>
+                  <Link href="/divanbed">
+                    <a>Linen Fabric Divan Beds</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/divanbed">
+                    <a>Velvet Fabric Divan Beds</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/divanbed">
+                    <a>Chennile Fabric Divan Bed</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/divanbed">
+                    <a>Suede Fabric Divan Bed</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/divanbed">
+                    <a>Low Divan Beds</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/divanbed">
+                    <a>Leather Divan Beds</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/divanbed">
+                    <a> Divan Bed Base</a>
+                  </Link>
+                </li>
+                <p>View All</p>
+              </ul>
+            </div>
+            </div>
+            <div className="col-md-3">
+            <div className={Styles.dropdownmenu}>
+              <h2>Shop BY Size</h2>
+              <ul>
+              <li>
+                  <Link href="/divanbed">
+                    <a>Small Single (2'6ft)</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/divanbed">
+                    <a>Single  (3ft)</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/divanbed">
+                    <a>Small Double  (4ft)</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/divanbed">
+                    <a>Double  (4'6ft)</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/divanbed">
+                    <a>King  (5ft)</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/divanbed">
+                    <a>Super King  (6ft)</a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            </div>
+            <div className="col-md-3">
+            <div className={Styles.dropdownmenu}>
+              <h2>Divan Beds Complete Set</h2>
+              <ul>
+              <li>
+                  <Link href="/divanbed">
+                    <a>Linen Fabric Divan Beds</a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            </div>
+          </div>
+        </div>
+      </section>
+                    </ul>
+                  </li>
+
+                  <li>
+                    <Link href="/divanbed">
+                      <a>All Beds </a> 
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/divanbed">
+                      <a>Headboards </a> 
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/divanbed">
+                      <a>Mattresses </a> 
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/divanbed">
+                      <a>Garden Furniture </a> 
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/divanbed">
+                      <a className={Styles.active}>Sale</a> 
+                    </Link>
+                  </li>               
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    
     </div>
   );
-
 };
 export default Header;
