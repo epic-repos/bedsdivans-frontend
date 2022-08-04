9dd1e48705b195c3db804970d3280c3d62961dbb
Asif3851 <aa9560323851@gmail.com>
2022-08-04 16:10:18 +0530

    changes

---
+++ pages/products/product.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "styles/product.module.scss";
+import Image from "next/image";
+import NextSEO from "layout/nextseo";
+import Header from "layout/header";
+
+const Divanbed = () => {
+  return (
+  <>
+
+<section className={styles.div1}>
+    <div className="container">
+        <div className="row">
+            <div className="col-6">jknfkjs</div>
+            <div className="col-6">aaaaaaaa</div>
+        </div>
+    </div>
+</section>
+  
+  </>
+  );    
+};
+export default Divanbed;
