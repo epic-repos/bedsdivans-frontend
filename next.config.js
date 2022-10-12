065681c2fe464a8f187c8d4f9dde62872eeb6b08
simpleneeraj <simplneeraj@gmail.com>
2022-10-12 12:33:56 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    swcMinify: true,
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-    images: {
-        domains: [
-            "www.bedsdivans.co.uk",
-            "cdn.shopify.com",
-            "localhost",
-            "api.dbzbeds.co.uk",
-        ],
-    },
+  reactStrictMode: false,
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: [
+      "www.bedsdivans.co.uk",
+      "cdn.shopify.com",
+      "localhost",
+      "api.dbzbeds.co.uk",
+    ],
+  },
 };
 
 module.exports = nextConfig;
