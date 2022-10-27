ee85a874599e00de9b211f59cd63ec217ba4656c
simpleneeraj <simplneeraj@gmail.com>
2022-10-27 17:23:48 +0530

    simple-cheanges

--- pages/cart/checkout.tsx
+++ pages/cart/checkout.tsx
@@ -323,7 +323,7 @@ const PaymentButton = ({
           onClick={onStripeCheckout}
           {...rest}
         >
-          Pay via Stripe Checkout
+          Pay via Checkout
         </button>
       );
     case "cash-on-delivery":
