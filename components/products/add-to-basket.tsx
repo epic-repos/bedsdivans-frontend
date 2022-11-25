f64e02217971b6962b8f0a0f9f0346859088e5cf
SahilMishraDev <msahil76640@gmail.com>
2022-11-25 14:53:50 +0530

    changes

--- components/products/add-to-basket.tsx
+++ components/products/add-to-basket.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import dynamic from "next/dynamic";
+import React, { useState } from "react";
 import css from "styles/product/page.module.scss";
+
+const SizeGuide = dynamic(() => import("components/products/size-guide"));
 
 interface AddToBasketProps {
   onChange: (value: number) => void;
@@ -17,12 +20,28 @@ const AddToBasket = ({ onClick, onChange }: AddToBasketProps) => {
     }
   }, [count]);
 
+  const [sizeGuide, setsizeGuide] = useState(false);
+
   React.useEffect(() => {
     onChange(count);
   }, [count]);
 
   return (
     <div>
+      {sizeGuide && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            height: "100%",
+            width: "100%",
+            background: "rgba(0,0,0,0.4)",
+          }}
+        >
+          <SizeGuide onClose={() => setsizeGuide(false)} />
+        </div>
+      )}
       <div className={css["basket-row"]}>
         <div className={css["basket-count"]}>
           <button onClick={decreaseCount}>-</button>
@@ -38,7 +57,12 @@ const AddToBasket = ({ onClick, onChange }: AddToBasketProps) => {
             ADD TO BASKET
           </button>
         </div>
-        <button className={css["size-guide"]}>SIZE GUIDE</button>
+        <button
+          className={css["size-guide"]}
+          onClick={() => setsizeGuide(true)}
+        >
+          SIZE GUIDE
+        </button>
       </div>
     </div>
   );
